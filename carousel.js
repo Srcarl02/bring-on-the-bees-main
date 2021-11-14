15eabd10125e2d86944d371a3c858fc75f1d0cdc
Sabrina Carlisle <carlisle.sabrina.sc@gmail.com>
2021-11-14 14:36:05 -0500

    working on carousel, having issue with js targetting all images on page

--- carousel.js
+++ carousel.js
@@ -1,3 +1,8 @@
+
+
+// /nav bar/
+
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav_menu");
 
@@ -17,18 +22,70 @@ function closeMenu() {
     navMenu.classList.remove("active");
 }
 
+//get all the images and li from HTML
+var carouselImages = document.getElementsByClassName("carouselImage");
+var dotNode = document.getElementsByTagName("li");
+var currentIndex = 0;
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("carousel_photo,");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+//create dots/circle
+for (let i = 0; i < carouselImages.length; i++) {
+  let newDot = document.createElement("li");
+  
+  newDot.className = "fa fa-circle";
+  newDot.setAttribute("onclick", "dotClick(this.id)");
+  newDot.setAttribute("id", parseInt(i));
+  
+  let dotContainer = document.querySelector(".dotList");
+  dotContainer.appendChild(newDot);
 }
+
+//function1
+function displayImage() {
+  for (let j = 0; j < carouselImages.length; j++) {
+    carouselImages[j].style.display = "none";
+  }
+  carouselImages[currentIndex].style.display = "block";
+}
+
+//function2
+function displayDot() {
+  for (let j = 0; j < dotNode.length; j++) {
+    dotNode[j].style.color = "grey";
+  }
+  dotNode[currentIndex].style.color = "black";
+}
+
+//function3
+function moveLeft() {
+  if (currentIndex == 0) {
+    currentIndex = carouselImages.length - 1;
+  } else {
+    currentIndex--;
+  }
+  
+  displayImage();
+  displayDot();
+}
+
+//function4
+function moveRight() {
+  if (currentIndex == carouselImages.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  
+  displayImage();
+  displayDot();
+}
+
+//function5
+function dotClick(c) {
+  currentIndex = c;
+  displayImage();
+  displayDot();
+}
+
+//initial stage
+displayImage();
+displayDot();
